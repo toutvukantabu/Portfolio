ee6e6cdf8259576d21563279a71d8ac55d03569a
toutvukantabu <gwendal.bescont@gmail.com>
2025-03-06 20:19:28 +0100

    feat: refactor ContactForm component; extract logic into custom hook for improved readability and maintainability

---
+++ src/domain/usecases/SubmitContactForm.ts
@@ -0,0 +1,11 @@
+import {ContactFormData} from "../models/ContactFormData";
+
+export const SubmitContactForm = async (data: ContactFormData): Promise<void> => {
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.log("Formulaire envoyé avec succès :", data);
+    } catch (error) {
+        console.error("Erreur lors de l’envoi du formulaire :", error);
+        throw error;
+    }
+};
--- src/domain/usecases/submitContactForm.ts
+++
@@ -1,6 +0,0 @@
-import {ContactFormData} from "@/domain/models/ContactFormData.ts";
-
-// @ts-ignore
-export const submitContactForm = async (data: ContactFormData): Promise<void> => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-};
